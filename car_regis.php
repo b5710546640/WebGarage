bbe69b1c458fdf938c78c733cab8198b797b17f9
Noot Jettananurak <Noot Jettananurak>
2016-04-29 21:31:28 +0700

    Update parking_slot and garbage

--- car_regis.php
+++ car_regis.php
@@ -200,7 +200,19 @@ else if(!empty($_POST['lc_number']) && !empty($_POST['car_brand']) && !empty($_P
         $sql2 = "INSERT INTO car (license, brand , person_id) VALUES ('$license', '$carBrand' , '$id_result')";
 
         $query2 = mysql_query($sql2);
-
+        
+        $slot_id = mysql_result(mysql_query("SELECT slot_id FROM parking_slot WHERE status = 'Available'"),0);
+        $query_update_parking_slot = mysql_query("UPDATE parking_slot SET status = 'Unavailable' WHERE slot_id = '$slot_id'");
+        
+        $garage_id = mysql_result(mysql_query("SELECT garage_id FROM parking_slot WHERE slot_id = '$slot_id'"),0);
+        $query_udpate_garage = mysql_query("UPDATE garage SET available_slot = available_slot-1 WHERE garage_id = '$garage_id'");
+        
+        $car_id = mysql_result(mysql_query("SELECT car_id FROM car WHERE license = '$license'"),0);
+        $sql_insert_parking_card = "INSERT INTO parking_card (slot_id,car_id) VALUE ('$slot_id','$car_id')";
+        $query = mysql_query($sql_insert_parking_card);
+        
+        header("Location:car_regist.php");
+        
     }
 }
 
