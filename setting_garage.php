edd862f69567d5b327bfdc1a50d25f6e7edabbda
Noot Jettananurak <Noot Jettananurak>
2016-04-29 18:37:07 +0700

    Can add parking slot to database

--- setting_garage.php
+++ setting_garage.php
@@ -9,8 +9,16 @@
                                             mysql_select_db("parking_registration") or die("Couldn't find database");
 
                                             $sql = "INSERT INTO garage (garage_name, capacity , available_slot) VALUES ('$name', '$capacity' , '$capacity')";
-
                                             $query = mysql_query($sql);
+                                            
+                                            $sql_garage_id = "SELECT garage_id From garage WHERE garage_name = '$name'";
+                                            $query_garage_id = mysql_result(mysql_query($sql_garage_id),0);
+                                            
+                                            for($i = 0 ; $i < $capacity ; $i++){
+                                                $sql_create_parking_slot = "INSERT INTO parking_slot (garage_id) VALUES ('$query_garage_id')";
+                                                $query = mysql_query($sql_create_parking_slot);
+                                            }
+                                            
                                             header("Location:setting.php");
                                         }
                                         else{
